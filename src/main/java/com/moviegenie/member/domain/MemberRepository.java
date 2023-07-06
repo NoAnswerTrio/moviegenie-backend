9e1d82675476910f71e18d01a906ce0d97a54d7f
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-06 19:26:34 +0900

    [#3] Feat : 회원 이메일 검증 기능 구현

--- src/main/java/com/moviegenie/member/domain/MemberRepository.java
+++ src/main/java/com/moviegenie/member/domain/MemberRepository.java
@@ -3,5 +3,10 @@ package com.moviegenie.member.domain;
 import com.moviegenie.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+    Optional<Member> findByEmail(String email);
 }
--- src/main/java/com/moviegenie/member/service/MemberService.java
+++ src/main/java/com/moviegenie/member/service/MemberService.java
@@ -5,6 +5,8 @@ import com.moviegenie.member.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MemberService {
@@ -12,5 +14,9 @@ public class MemberService {
     private final MemberRepository memberRepository;
     public void signUp(Member member) {
         memberRepository.save(member);
+    }
+
+    public boolean isDuplicatedEmail(String email) {
+        return memberRepository.existsByEmail(email);
     }
 }
