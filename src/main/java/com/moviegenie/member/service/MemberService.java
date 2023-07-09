da2f2eeb884def39256c7269791bb22b494b089c
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-09 17:14:46 +0900

    [#5] Feat : 회원 로그인 정보 검증 기능 구현

--- src/main/java/com/moviegenie/member/service/MemberService.java
+++ src/main/java/com/moviegenie/member/service/MemberService.java
@@ -1,11 +1,13 @@
 package com.moviegenie.member.service;
 
+import com.moviegenie.member.controller.dto.MemberLoginDto;
 import com.moviegenie.member.domain.MemberRepository;
 import com.moviegenie.member.domain.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +20,16 @@ public class MemberService {
 
     public boolean isDuplicatedEmail(String email) {
         return memberRepository.existsByEmail(email);
+    }
+
+    public boolean isValidLoginInfo(MemberLoginDto loginDto) {
+        Optional<Member> loginMember = memberRepository.findByEmail(loginDto.getEmail());
+
+        if (loginMember.isPresent()) {
+            if (loginMember.get().getPassword().equals(loginDto.getPassword())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
