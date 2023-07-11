e8760a8c209377a2777d980ac81714d21cec2228
tlqkrus012345 <tlqkrus012345@naver.com>
2023-07-11 23:07:38 +0900

    [#10] Feat : ErrorCode enum 구현

---
+++ src/main/java/com/moviegenie/exception/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.moviegenie.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    DUPLICATED_EMAIL(HttpStatus.CONFLICT, "중복된 이메일입니다."),
+    MEMBER_EMAIL_NOT_FOUND(HttpStatus.NOT_FOUND, "입력하신 이메일은 없는 이메일입니다."),
+    ;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
